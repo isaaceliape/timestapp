b9c74cef01176fd15c453608073253ff7d006907
Isaac Eliape <hello@isaaceliape.com>
2014-07-20 20:50:47 -0300

    structure done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+$(document).ready(function () {
+	$(".bt-menu").click(function(){
+		$(".menu").addClass("show");	
+		$(".logo").addClass("grey");	
+	});
+	$(".bt-close").click(function(){
+		$(".menu").removeClass("show");	
+		setTimeout(function() {
+			$(".logo").removeClass("grey");
+		}, 800);
+	});
 
+	$(".menu a, .logo").click(function(e){
+		var page = e.currentTarget.hash;
+		page = page.replace('#','');
+
+		if(!$('.current').hasClass(page)){
+			if(page != "log-out"){
+				$(".menu").removeClass("show");	
+				$(".logo").removeClass("grey");	
+
+				$(".current").removeClass("current").addClass("older");
+				$(".page."+page).addClass("next");
+				setTimeout(function() {
+					$(".next").removeClass("next").addClass("current");
+					$(".older").removeClass("older");
+				}, 1500);
+			}
+		}
+		return false;
+	});
+})
