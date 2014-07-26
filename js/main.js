bfa65b7f8a2a47b39ce20af216003aa4c0083fac
Isaac Eliape <hello@isaaceliape.com>
2014-07-26 06:06:15 -0300

    First part of implementation of the chart

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-$(document).ready(function () {
-	// Initilize site
-	initializa();
 
-	// BIND EVENTS
-	$(".menu a, .logo").click(function(e){changePage(e);return false;});
-	$(".bt-menu").click(openMenu);
-	$(".menu .bt-close").click(closeMenu);
-	$(".tasks .first").click(function() {
-		changePage(null, '#new-task');
-	});
-	$('.login-form').submit(function(){
-		//Clear all the inputs
-		$(this).children('input').each(function(){
-			$(this).val('');
+var criateDial = function(){
+	$(".dial")
+		.knob({
+			 // readOnly : true,
+			'width'     : 210,
+			'height'    : 210,
+			'readOnly'  : true,
+			'lineCap'   : 'round',
+			'thickness' : '.15',
+			'fgColor'   : 'white',
+			'bgColor'   : 'transparent',
+			'rotation'  : 'anticlockwise',
+			'displayInput' : false
 		});
-		
-		changePage(null, '#tasks');
-		return false;
-	});
-})
+};
 
 var addInputMasks = function () {
 	$(".cash-mask").mask("$ 99?999",{placeholder:" "});
 	$(".time-mask").mask("?99:99",{placeholder:"0"});
-}
+};
 var openMenu = function(){
 	$(".menu").addClass("show");	
 	$(".logo").addClass("grey");
-}
+};
 var closeMenu = function() {
 	$(".menu").removeClass("show");	
+
 	setTimeout(function() {
 		$(".logo").removeClass("grey");
 	}, 800);
-}
+};
 var changePage = function(e, hash){
 	var page;
 	if(hash){
@@ -46,7 +42,7 @@ var changePage = function(e, hash){
 
 
 	if(!$('.current').hasClass(page)){
-		if(page != "log-out"){
+		if(page !== "log-out"){
 			$(".menu").removeClass("show");	
 			$(".logo").removeClass("grey");	
 
@@ -58,8 +54,48 @@ var changePage = function(e, hash){
 			}, 1500);
 		}
 	}
-}
-var initializa = function() {
-	addInputMasks();
-}
+};
 
+$(document).ready(function () {
+	addInputMasks();
+	criateDial();
+
+	displayTime = 55;
+	var d = new Date();
+	countDown = function(message){
+		displayTime--;
+		if(displayTime < 50){
+			$('.new-task .box').removeClass('blue').addClass('red');
+		}
+		d.setTime(displayTime*1000);
+		$('.txt-time').val(d.getMinutes()+':'+d.getSeconds());
+		$('.progress-bar .fill').width(displayTime+'%');
+		$('.fill .txt-budget').val("$ "+displayTime);
+		$('.dial').val(displayTime).trigger('change');
+
+		if (displayTime == 0){
+			clearInterval(timer);
+			// alert("Times Up!");
+		}
+	}
+	timer = setInterval(countDown, 1000);
+
+
+});
+
+// BIND EVENTS
+$(".menu a, .logo").click(function(e){changePage(e);return false;});
+$(".bt-menu").click(openMenu);
+$(".menu .bt-close").click(closeMenu);
+$(".tasks .first").click(function() {
+	changePage(null, '#new-task');
+});
+$('.login-form').submit(function(){
+	//Clear all the inputs
+	$(this).children('input').each(function(){
+		$(this).val('');
+	});
+	
+	changePage(null, '#tasks');
+	return false;
+});
